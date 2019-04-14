55a730f82a3e75aaa59036f1d32cc2603a035df9
AnranLi <al3804@columbia.edu>
2019-04-14 17:32:11 -0400

    No 117

---
+++ 117_PopulatingNextRightPointersInEachNodeII.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+from collections import deque
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return
+        queue = deque()
+        queue.append(root)
+        while queue:
+            length = len(queue)
+            for i in range(length):
+                curr = queue.popleft()
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+                if i == length - 1:
+                    continue
+                curr.next = queue[0]
+        return root
+        
