d0bc23d0f501fada53bd375259225ed5bce71e60
AnranLi <al3804@columbia.edu>
2019-04-30 15:11:57 -0400

    No 86

---
+++ 86PartitionList.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def partition(self, head: ListNode, x: int) -> ListNode:
+        small, big = ListNode(0), ListNode(0)
+        dummy1, dummy2 = small, big
+        while head:
+            if head.val < x:
+                small.next = head
+                small = small.next
+            else:
+                big.next = head
+                big = big.next
+            head = head.next
+        small.next = dummy2.next
+        big.next = None
+        return dummy1.next
+        
