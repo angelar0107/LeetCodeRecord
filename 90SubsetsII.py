4accbf4e3512033416e90151aedc1797ef99356f
AnranLi <al3804@columbia.edu>
2019-07-04 20:48:19 +0800

    Add Leetcode temp ver.

--- 90SubsetsII.py
+++ 90SubsetsII.py
@@ -17,4 +17,26 @@ class Solution:
             use[i] = True
             self.dfs(nums,use,result,curr + [nums[i]], i + 1)
             use[i] = False
-        
+
+
+#Leetcode Template Ver.
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        used = [False for _ in nums]
+        self.dfs(nums,res,used,0,[])
+        return res
+    
+    def dfs(self,nums,res,used,index,curr):
+        res.append(list(curr))
+        for i in range(index,len(nums)):
+            if i > 0 and nums[i] == nums[i - 1] and used[i - 1] == False:
+                continue
+            curr.append(nums[i])
+            used[i] = True
+            self.dfs(nums,res,used,i + 1, curr)
+            curr.pop()
+            used[i] = False
+            
+                
